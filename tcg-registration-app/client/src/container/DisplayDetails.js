441789c417208d325a2a4cec5b284c2326b62051
maggiemoteno2 <motenomaggie@gmail.com>
2020-09-04 10:51:50 +0200

    added a form that allows a students to enter their personal data

--- tcg-registration-app/client/src/container/DisplayDetails.js
+++ tcg-registration-app/client/src/container/DisplayDetails.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-function DisplayDetails() {
-  const [credentials, setCredentials] = useState({ name: "", surname: "", email: "", password: "",
-   home_Address:"", id_Number:""})
+function DisplayDetails () {
+  const [credentials, setCredentials] = useState({
+    name: "", surname: "", email: "", password: "",
+    home_Address: "", id_Number: ""
+  })
   const [studentDetails, setStudentDetails] = useState([])
 
   useEffect(async () => {
@@ -25,48 +27,71 @@ function DisplayDetails() {
   }
   return (
     <div>
-      <form onSubmit={(e) => submit(e)}>
-        <input type="name"
-          name="name"
-          value={credentials.name}
-          placeholder="enter your name"
-          onChange={(e) => setCredentials({ ...credentials, [e.target.name]: e.target.value })} />
+      <div class="signup-form">
+        <form onSubmit={(e) => submit(e)}>
+          <h2>Fill Form</h2>
+          <p class="hint-text">Enter Personal Details</p>
+          <div class="form-group">
+            <div class="row">
+              <div class="col"> <input type="name" class="form-control"
+                name="name"
+                value={credentials.name}
+                placeholder="enter your name"
+                onChange={(e) => setCredentials({ ...credentials, [e.target.name]: e.target.value })} /></div>
+            </div>
+          </div>
 
-        <input type="surname"
-          name="surname"
-          value={credentials.lastName}
-          placeholder="enter your lastName"
-          onChange={(e) => setCredentials({ ...credentials, [e.target.name]: e.target.value })} />
-        <br />
-        <input type="email"
-          name="email"
-          value={credentials.email}
-          placeholder=" enter email"
-          onChange={(e) => setCredentials({ ...credentials, [e.target.name]: e.target.value })} />
-        <br />
-        <input name="id_Number"
-          value={credentials.id_Number} type="text" placeholder="id_Number"
-          onChange={(e) => setCredentials({ ...credentials, [e.target.name]: e.target.value })} />
-        <br />
-        <input name="home_Address"
-          value={credentials.home_Address}
-          type="text" onChange={(e) => setCredentials({ ...credentials, [e.target.name]: e.target.value })} placeholder="home_Address"/>
-        <br />
-        <input name="password"
-          value={credentials.password} type="password" placeholder="enter password"
-          onChange={(e) => setCredentials({ ...credentials, [e.target.name]: e.target.value })} />
-        <br />
-        <input type="submit" />
-      </form>
-      {studentDetails.map(credential => <div>Name:{credential.name}, surname:{credential.surname}, email:{credential.email},
-       Home Address:{credential.home_Address}, ID number:{credential.id_Number}<br/><br/>
-       </div>)
-      }
-     
+          <div class="form-group">
+            <input type="surname"
+              class="form-control"
+              name="surname"
+              value={credentials.lastName}
+              placeholder="enter your lastName"
+              onChange={(e) => setCredentials({ ...credentials, [e.target.name]: e.target.value })} />
+          </div>
+          <br />
+          <div class="form-group">
+            <input type="email"
+              class="form-control"
+              name="email"
+              value={credentials.email}
+              placeholder=" enter email"
+              onChange={(e) => setCredentials({ ...credentials, [e.target.name]: e.target.value })} />
+          </div>
+          <br />
+          <div class="form-group">
+          <input name="id_Number"
+              class="form-control"
+            value={credentials.id_Number} type="text" placeholder="id_Number"
+            onChange={(e) => setCredentials({ ...credentials, [e.target.name]: e.target.value })} />
+          </div>
+          <br />
+          <div class="form-group">
+            <input name="home_Address"
+              class="form-control"
+              value={credentials.home_Address}
+              type="text" onChange={(e) => setCredentials({ ...credentials, [e.target.name]: e.target.value })} placeholder="home_Address" />
+          </div>
+          <br />
+          <div class="form-group">
+          <input name="password"
+              class="form-control"
+            value={credentials.password} type="password" placeholder="enter password"
+            onChange={(e) => setCredentials({ ...credentials, [e.target.name]: e.target.value })} />
+           </div>
+          <br />
+          <div class="form-group">
+            <input class="btn btn-success btn-lg btn-block" type="submit" />
+          </div>
+        </form>
+        {studentDetails.map(credential => <div>Name:{credential.name}, surname:{credential.surname}, email:{credential.email},
+       Home Address:{credential.home_Address}, ID number:{credential.id_Number}<br /><br />
+        </div>)
+        }
+      </div>
     </div>
   )
 }
 
 export default DisplayDetails
 
- 
